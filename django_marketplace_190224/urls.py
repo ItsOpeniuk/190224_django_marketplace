fb8d3510da961a78926c492e6bacff7b6eb95814
Dimm <slim.slk@gmail.com>
2024-06-26 14:14:42 +0200

    add swagger endpionts

--- django_marketplace_190224/urls.py
+++ django_marketplace_190224/urls.py
@@ -16,9 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
+schema_view = get_schema_view(
+    info=openapi.Info(
+        title='Marketplace',
+        default_version='v1',
+        description='API to CRUD operations',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(
+            name='Dimm',
+            email='dimm@marketplace.com'
+        ),
+        license=openapi.License(
+            name='MIT License'
+        ),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('marketplace.urls'))
+    path('api/v1/', include('marketplace.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=1000)),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=1000)),
 ]
